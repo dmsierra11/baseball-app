076aa5a9755e867d882f66c582fee30663c7d3a6
Daniel Sierra <danieluchin@gmail.com>
2024-07-21 14:50:40 +0200

    Update team standings endpoint

--- src/hooks/useStandings.tsx
+++ src/hooks/useStandings.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import { BASE_URL } from '../config/api';
-import type { TeamStandingsType } from 'fantasy-baseball-ui';
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../config/api";
+import type { TeamStandingsType } from "fantasy-baseball-ui";
 
 export const useStandings = () => {
-    const [standings, setStandings] = useState<TeamStandingsType[]>([]);
+  const [standings, setStandings] = useState<TeamStandingsType[]>([]);
 
-    useEffect(() => {
-        fetch(`${BASE_URL}/standings`)
-            .then((response) => response.json())
-            .then((data) => setStandings(data as unknown as TeamStandingsType[]));
-    }, []);
+  useEffect(() => {
+    fetch(`${BASE_URL}/teams?type=standings`)
+      .then((response) => response.json())
+      .then((data) => setStandings(data as unknown as TeamStandingsType[]));
+  }, []);
 
-    return standings;
-}
+  return standings;
+};
