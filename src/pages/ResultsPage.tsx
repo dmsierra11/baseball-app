a8664c357915517edbad4f42efcec1ae12e39153
Daniel Sierra <danieluchin@gmail.com>
2024-07-25 02:03:49 +0200

    Fixes tsc issue

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -5,8 +5,8 @@ const ResultsPage = () => {
     <Container>
       <GameCard
         date="2024-05-01"
-        homeTeam={{ name: "LAD", score: 5, record: "1-0" }}
-        awayTeam={{ name: "SF", score: 3, record: "0-1" }}
+        home_team={{ name: "LAD", score: 5, record: "1-0" }}
+        away_team={{ name: "SF", score: 3, record: "0-1" }}
         location="Campo 1"
       />
     </Container>
